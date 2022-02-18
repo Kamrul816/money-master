a5b1723d76d78b27bdb3f28b7675373773a28a58
Kamrul816 <freelancerkamrul816@gmail.com>
2022-02-18 23:21:40 +0600

    expenses update

--- js/account.js
+++ js/account.js
@@ -1,27 +1,47 @@
+// handle expenses button event 
+document.getElementById('calculation-button').addEventListener('click',function(){
+    
+    // get food amount expenses
+     const foodInput = document.getElementById('food-input');
+     const foodAmount = foodInput.value;
+     console.log(foodAmount);
+     const expensesTotal = document.getElementById('expenses-total');
+     expensesTotal.innerText = foodAmount;
+
+    // clear the food input field 
+    foodInput.value = '';
+});
+
 // handle expenses button event
 document.getElementById('calculation-button').addEventListener('click',function(){
     
-    // get food amount expenses 
-    const expensesInput = document.getElementById('food-input');
-    const expensesAmount = expensesInput.value;
-    console.log(expensesAmount);
+    // get rent amount expenses
+    const rentInput = document.getElementById('rent-input');
+    const rentAmount = rentInput.value;
+    console.log(rentAmount);
     const expensesTotal = document.getElementById('expenses-total');
-    expensesTotal.innerText = expensesAmount;
+    expensesTotal.innerText = rentAmount;
 
     // clear the food input field 
-    expensesInput.value = '';
+    rentInput.value = '';   
 });
-// handle expenses button event
+
+// handle expenses button event 
 document.getElementById('calculation-button').addEventListener('click',function(){
-    // get rent amount expenses
-    const expensesInput = document.getElementById('rent-input');
-    const expensesAmount = expensesInput.value;
-    console.log(expensesAmount);   
-});
-// handle expenses button event
-document.getElementById('calculation-button').addEventListener('click',function(){
+    
     // get clothes amount expenses
-     const expensesInput = document.getElementById('clothes-input');
-     const expensesAmount = expensesInput.value;
-     console.log(expensesAmount);
+     const clothesInput = document.getElementById('clothes-input');
+     const clothesAmount = clothesInput.value;
+     console.log(clothesAmount);
+     const expensesTotal = document.getElementById('expenses-total');
+     expensesTotal.innerText = clothesAmount;
+
+    // clear the food input field 
+    clothesInput.value = '';
 });
+
+document.getElementById('calculation-button').addEventListener('click',function(){
+    const expensesTotal = foodAmount + rentAmount + clothesAmount;
+    console.log(expensesTotal);
+
+})
